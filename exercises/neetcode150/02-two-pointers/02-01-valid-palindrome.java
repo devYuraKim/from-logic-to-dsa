8e597ec246ff03b9964821ae7b31c757e0c04741
devYuraKim <dev.yurakim@gmail.com>
2026-05-09 01:15:12 +0900

    practice: NC150 [02 Two Pointers] 01. Valid Palindrome

--- exercises/neetcode150/02-two-pointers/02-01-valid-palindrome.java
+++ exercises/neetcode150/02-two-pointers/02-01-valid-palindrome.java
@@ -1,31 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String lowerCase = s.toLowerCase();
-        char[] charArray = lowerCase.toCharArray();
 
-        int leftPointer = 0;
-        int rightPointer = charArray.length-1;
+        int left = 0;
+        int right = s.length()-1;
 
-        while(leftPointer < rightPointer){ //이렇게 해야 leftPointer++ 했을 때 rightPointer랑 같아질 수 있으니까
-            //while(leftPointer <= rightPointer){
-            while(leftPointer < rightPointer && !Character.isLetterOrDigit(charArray[leftPointer])){
-                //if(leftPointer < rightPointer) leftPointer++; // 이거 했더니 Inifinite Loop 발생
-                leftPointer++;
-            }
-            while(leftPointer < rightPointer && !Character.isLetterOrDigit(charArray[rightPointer])){
-                //if(leftPointer < rightPointer) rightPointer--;
-                rightPointer--;
-            }
-
-            if(charArray[leftPointer]==charArray[rightPointer]){
-                leftPointer++;
-                //rightPointer++;
-                rightPointer--;
+        while(left <= right){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }else if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
             }else{
-                return false;
+                if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                    return false;
+                }
+                left++;
+                right--;
             }
         }
-
         return true;
 
     }
