54a6874ee28c55a37d3550eb27671c831c99ed50
devYuraKim <dev.yurakim@gmail.com>
2026-05-10 15:15:57 +0900

    practice: NC150 [02 Two Pointers] 03. 3 Sum

--- exercises/neetcode150/02-two-pointers/02-03-3-sum.java
+++ exercises/neetcode150/02-two-pointers/02-03-3-sum.java
@@ -1,47 +1,44 @@
-//Sat May 09 2026
-//37'54''
+//Sun May 10 2026
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
-        Arrays.sort(nums); // [-4, -1, -1, 0, 1, 2]
+        Arrays.sort(nums);
 
         List<List<Integer>> output = new ArrayList<>();
 
-        for(int i=0; i<nums.length-2; i++){
+        // [1오류] 순회 i 종료 조건
+        // for(int i=0; i<nums.length-2; i++){
+        for(int i=0; i<nums.length; i++){
+
+            // [2오류] early break
+            // if (nums[i] > 0) break;
 
             int left = i+1;
             int right = nums.length-1;
 
-            if(i>=1 && nums[i]==nums[i-1]) continue;
+            // [3오류] nums[i] 중복 처리
+            // if(i > 0 && nums[i]==nums[i-1]) continue;
 
-            while(left<right){
-                int sum = nums[left]+nums[i]+nums[right];
-
-                if(sum < 0){
-                    while(left<right && nums[left]==nums[left+1]){
+            while(left < right){
+                if(nums[i]+nums[left]+nums[right] == 0){
+                    // [4오류] Arrays.asList()
+                    //List<Integer> tempList = Arrays.asList(nums[i], nums[left], nums[right]);
+                    List<Integer> tempList = List.asList(nums[i], nums[left], nums[right]);
+                    output.add(new ArrayList<>(tempList));
+                    // [5오류] left, right는 '이후 원소'와 비교, i는 '이전 원소'와 비교
+                    //while(left < right && nums[left]==nums[left+1]
+                    while(left < right && nums[left-1]==nums[left]) {
                         left++;
                     }
                     left++;
-                } else if(sum > 0){
-                    while(left<right && nums[right]==nums[right-1]){
+                    while(left < right && nums[right-1]==nums[right]) {
                         right--;
                     }
                     right--;
-                } else {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[left]);
-                    temp.add(nums[i]);
-                    temp.add(nums[right]);
-                    output.add(new ArrayList<>(temp));
-
-                    while(left<right && nums[left]==nums[left+1]){
-                        left++;
-                    }
+                }else if(nums[i]+nums[left]+nums[right] > 0){
+                    right--;
+                }else{
                     left++;
-                    while(left<right && nums[right]==nums[right-1]){
-                        right--;
-                    }
-                    right--;
                 }
             }
 
