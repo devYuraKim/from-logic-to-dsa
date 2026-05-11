3c6815da112d5eac94b50c4bf272e15118d690e3
devYuraKim <dev.yurakim@gmail.com>
2026-05-11 14:39:42 +0900

    practice: NC150 [04 Binary Search] 02. Search a 2D Matrix

--- exercises/neetcode150/04-binary-search/04-02-search-a-2d-matrix.java
+++ exercises/neetcode150/04-binary-search/04-02-search-a-2d-matrix.java
@@ -1,149 +1,54 @@
-// Sat Apr 25
-// 1:16:02
+// Mon May 11 2026
+// 44:36
 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        // (r,c) = (m,n)
+        int rowCount = matrix.length;
+        int colCount = matrix[0].length;
+
+        int left = 1;
+        int right = rowCount * colCount;
+
+        // 각 칸의 좌표 어떻게 구하지?
         // (0,0) (0,1) (0,2) (0,3)
         // (1,0) (1,1) (1,2) (1,3)
-        // (2,0) (2,1) (2,2) (2,3)
-        // (0,0) - (m-1, n-1)
-
-        // 이렇게 하지 말고
-        // up, down
-        // left, right
-        // 이렇게 하면 어떨까
-        // 하는 생각이 14:36에 들었음
-
-        // 아니면
-        // up, down
-        // left, right을
-        // 각각 binary search로 하는 건 어떨까
-        // 하는 생각이 24:58에 들었음
 
 
-        // int row = -1;
-        // //row 선택
-        // for(int i = 0; i < matrix.length; i++){
-        //     if(matrix[i][0] < target) {
-        //         continue;
-        //     }else if(matrix[i][0] = target){
-        //         return true;
-        //     }else{
-        //         row = i-1;
-        //         break;
-        //     }
-        // }
-        // // column 선택
+        // int midRow = (0+rowCount-1)/2;
+        // int midCol = (0+colCount-1)/2;
 
-
-
-        // int down = 0;
-        // int up = matrix.length - 1;
-
-        // int right = 0;
-        // int left = matrix[0].length - 1;
-
-        // while(down <= up){
-        //    if(matrix[down][0] < target){
-        //     down++;
-        //    }
-
-        //    if(matix[up][0] > target){
-        //     up--;
-        //    }
-        // }
-
-
-
-        //row binary
-        // int startRow = 0;
-        // int endRow = matrix.length - 1;
-
-        // 그런데 이렇게 하면 범위를 좁힐 수가 없는데...?
-        // int halfRow = ( 0 + matrix.length-1 )/2;
-        // if(matrix[halfRow][0] > target){
-
-        // }else if(matrix[row/2][0] == target){
+        // if(target == matrix[midRow][midCol]){ //matrix[1][1]
         //     return true;
-        // }else{
-
+        //     // 6번째라면 
+        //     // 6/colCount = 몫 = row
+        //     // 6/colCount = 나머지 > 1 때는 -1, 아닐 때는 마지막 column = col
+        // }else if(target > matrix[midRow][midCol]){
+        //     midRow = (midRow+rowCount-1)/2;
         // }
 
-        // (0) 1
-        // (1) 10 11 12 13 14
-        // (2) 15
-        // (3) 22
-        // (4) 30
-        // (5) 45
-        // (6) 56 57 58 59 60
-        // (7) 62
-        // (8) 78
 
-        // target == 58
-        // halfRow -> 4 (30) < 58
-        // endRow 고정, startRow = halfRow+1 (5+8)/2 = 6
-        // halfRow -> 6 (56) < 58
-        // endRow 고정, startRow = halfRow+1 (7+8)/2 = 7
-        // halfRow -> 7 (62) > 58
-        // FLAG: 6과 7 사이이므로 row 6에서 검색
+        // cellNumber -> matrix[r][c]
+        // (1) row: cellNumber / colCount;
+        // (2) col: cellNumber % colCount;
+        //          if(cellNumber%colCount==0) col=matrix[0].length-1;
+        //          else col=cellNumber%colCount-1;
 
-        // target == 13
-        // halfRow -> 4 (30)
-        // startRow 고정, endRow = halfRow-1 (0+3)/2 = 1
-        // halfRow -> 1 (10) < 13
-        // endRow 고정 3, startRow = halfRow+1 (3+2) = 2
-        // halfRow -> 2 (15) > 13
-        // FLAG: 1과 2 사이이므로 row 1에서 검색
 
-        // flag를 isReversed로 관리, 바뀌는 지점에서 row 확정
-        // AI들의 도움을 받음. 굳이 그렇게 안 해도 된다고 함.
+        while(left <= right){
+            int half = (left+right) / 2;
 
-        // int startRow = 0;
-        // int endRow = matrix.length - 1;
+            int row = half % colCount == 0 ? half/colCount - 1 : half / colCount;
+            int col = half % colCount == 0 ? colCount-1 : half % colCount -1;
 
-        // int halfRow = (startRow + endRow)/2;
-        // int endCol = matrix[0].length - 1;
-
-        // while(matrix[halfRow][0] <= target && matrix[halfRow][endCol] >= target){
-        //     if(matrix[halfRow][0] > target){
-
-        //     }
-        // }
-
-        int start = 0;
-        int end = matrix.length - 1;
-        int endCol = matrix[0].length-1;
-        int targetRow = -1;
-
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-
-            if (matrix[mid][0] <= target && target <= matrix[mid][endCol]) {
-                targetRow = mid; // 찾았다!
-                break;
-            } else if (matrix[mid][0] > target) {
-                end = mid - 1; // 너무 아래 행이다, 위로 가자
-            } else {
-                start = mid + 1; // 너무 위 행이다, 아래로 가자
-            }
-        }
-
-        if (targetRow == -1) return false; // 어떤 행에도 포함될 수 없음
-
-        int left = 0;
-        int right = matrix[targetRow].length - 1;
-
-        while (left <= right){
-            int mid = left + (right-left)/2;
-
-            if(matrix[targetRow][mid] == target){
+            if(target == matrix[row][col]){
                 return true;
-            }else if(matrix[targetRow][mid] > target){
-                right = mid-1;
-            }else{
-                left = mid+1;
+            }else if(target > matrix[row][col]){
+                left = half+1;
+                // half = (left+right)/2;
+            }else {
+                right = half-1;
+                // half = (left+right)/2;
             }
         }
 
@@ -151,3 +56,5 @@ class Solution {
 
     }
 }
+
+// m * n = r * c
--- exercises/neetcode150/04-binary-search/04-02-search-a-2d-matrix.md
+++ exercises/neetcode150/04-binary-search/04-02-search-a-2d-matrix.md
@@ -1,25 +1,84 @@
+> **문제1**: 
+> `while(left < right)`     
+> `left == right`일 때, 즉 탐색 범위가 원소 하나로 좁혀졌을 때 루프가 종료되어 해당 원소를 확인하지 못하고 `false`를 반환함. `<=`로 수정해야 마지막 원소까지 체크 가능.
+
+> **문제2**:
+> `int row = half / colCount;`     
+> `1-based index`를 쓸 경우, `half`가 `colCount`의 배수일 때(= 각 행의 마지막 원소) `half / colCount`가 실제 `row`보다 1 크게 계산됨.     
+> 예: `colCount=4`, `half=4`이면 `row`는 `0`이어야 하는데 1이 나옴. `half % colCount == 0`인 경우 `half/colCount - 1`로 처리해야 함.
+
+> **문제3**: 
+> `half = (left+right)/2;`     
+> `left` 또는 `right`를 갱신한 직후 루프 내부에서 `half`를 재계산하고 있으나, 이 값은 이후 코드에서 사용되지 않고 다음 iteration 첫 줄에서 어차피 덮어씌워짐. 완전히 불필요한 코드.
+
+> **문제4**:
+> `int col = half % colCount == 0 ? col = colCount-1 : half % colCount -1;`     
+> 삼항 연산자 내부에서 다시 대입 `col =` 사용.
+
+
+### Original Code
+
 ```java
-int m = matrix.length;
-int n = matrix[0].length;
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
 
-int left = 0;
-int right = m * n - 1;
+        int rowCount = matrix.length;
+        int colCount = matrix[0].length;
 
-while (left <= right) {
-    int mid = left + (right - left) / 2;
-    
-    int row = mid / n;
-    int col = mid % n;
+        int left = 1;
+        int right = rowCount * colCount;
+        
+        while(left < right){
+            int half = (left+right) / 2;
 
-    if (matrix[row][col] == target) {
-        return true;
-    } else if (matrix[row][col] < target) {
-        left = mid + 1;
-    } else {
-        right = mid - 1;
+            int row = half / colCount;
+            int col = half % colCount == 0 ? col = colCount-1 : half % colCount -1;
+
+            if(target == matrix[row][col]){
+                return true;
+            }else if(target > matrix[row][col]){
+                left = half+1;
+                 half = (left+right)/2;
+            }else {
+                right = half-1;
+                 half = (left+right)/2;
+            }
+        }
+
+        return false;
+
     }
 }
-
-return false;
 ```
 
+### Corrected Code
+```java
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int rowCount = matrix.length;
+        int colCount = matrix[0].length;
+
+        int left = 1;
+        int right = rowCount * colCount;
+
+        while(left <= right){
+            int half = (left+right) / 2;
+
+            int row = half % colCount == 0 ? half/colCount - 1 : half / colCount;
+            int col = half % colCount == 0 ? colCount-1 : half % colCount -1; 
+
+            if(target == matrix[row][col]){
+                return true;
+            }else if(target > matrix[row][col]){
+                left = half+1;
+            }else {
+                right = half-1;
+            }
+        }
+
+        return false;
+
+    }
+}
+```
