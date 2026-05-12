296e941b18c2a82d50348ea123762f3bd16b195f
devYuraKim <dev.yurakim@gmail.com>
2026-05-12 14:13:25 +0900

    practice: NC150 [09 Backtracking] 01. Subsets

--- exercises/neetcode150/09-backtracking/09-01-subsets.java
+++ exercises/neetcode150/09-backtracking/09-01-subsets.java
@@ -1,39 +1,31 @@
-// 6'48''
-// 주의1: depth == nums.length 맞음 off by one 유념.
-    // depth 0 -> 1 결정
-    // depth 1 -> 2 결정
-    // depth 2 -> 3 결정
-    // depth 3 == nums.length -> 결과 확정
-// 주의2: output.add(tempList)할 때 그냥 하면 오류 발생.
-
+// Tue May 12 2026
+// 11:51
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
 
         List<List<Integer>> output = new ArrayList<>();
         List<Integer> tempList = new ArrayList<>();
-        recursion(nums, tempList, output, 0);
+
+        recursive(nums, output, tempList, 0);
 
         return output;
 
     }
 
-    public void recursion(int[] nums, List<Integer> tempList, List<List<Integer>> output, int depth){
+    public void recursive(int[] nums, List<List<Integer>> output, List<Integer> tempList, int startIndex){
 
-        // 트리 바닥 도달: 모든 층의 포함/미포함 결정이 끝났으므로 output에 추가
-        // off by one 주의: 트리 바닥 도달한 다음 단계에서 추가해야 하므로 depth == nums.length 맞음
-        if(depth == nums.length){
+        if(startIndex == nums.length) {
             output.add(new ArrayList<>(tempList));
             return;
         }
 
-        // depth번째 원소를 포함하는 모든 경우 탐색
-        tempList.add(nums[depth]);
-        recursion(nums, tempList, output, depth+1);
+        // 해당 원소 포함
+        tempList.add(nums[startIndex]);
+        recursive(nums, output, tempList, startIndex+1);
 
-        // depth번째 원소 미포함하는 모든 경우 탐색
-        tempList.remove(tempList.size() - 1);
-        recursion(nums, tempList, output, depth+1);
+        // 해당 원소 미포함
+        tempList.remove(tempList.size()-1);
+        recursive(nums, output, tempList, startIndex+1);
 
     }
-
 }
