9fbdb1b58825c449f768442550a3a0db4bb2938d
devYuraKim <dev.yurakim@gmail.com>
2026-05-12 14:13:43 +0900

    practice: NC150 [09 Backtracking] 02. Combination Sum

--- exercises/neetcode150/09-backtracking/09-02-02-combination-sum.java
+++ exercises/neetcode150/09-backtracking/09-02-02-combination-sum.java
@@ -1,3 +1,5 @@
+// Tue May 12 2026
+// 08:59
 class Solution {
     public List<List<Integer>> combinationSum(int[] nums, int target) {
 
@@ -12,25 +14,22 @@ class Solution {
 
     public void recursion(int[] nums, int target, List<List<Integer>> output, List<Integer> tempList, int startIndex){
 
-        //종료조건
-        if(target == 0) {
+        if(target == 0){
             output.add(new ArrayList<>(tempList));
             return;
         }
 
-        //early return
-        if(target < 0 || startIndex == nums.length) {
+        if(target < 0 || startIndex >= nums.length){
             return;
         }
 
-        // nums[startIndex] 원소 포함: 같은 후보 유지
+        //해당 원소를 선택
         tempList.add(nums[startIndex]);
         recursion(nums, target-nums[startIndex], output, tempList, startIndex);
 
-        // nums[startIndex] 원소 미포함: 다음 후보로 이동
+        //해당 원소를 미선택
         tempList.remove(tempList.size()-1);
         recursion(nums, target, output, tempList, startIndex+1);
 
     }
-
 }
