576218884a153fb8e879c82b3f048414c29bd6a1
devYuraKim <dev.yurakim@gmail.com>
2026-05-13 23:13:57 +0900

    practice: NC150 [03 Stack] 01. Valid Parentheses

--- exercises/neetcode150/03-stack/03-01-valid-parentheses.java
+++ exercises/neetcode150/03-stack/03-01-valid-parentheses.java
@@ -1,81 +1,31 @@
-// Approach 1 - [{()}] 이 test case만 보고 코드 작성함
+//10'17''
 class Solution {
     public boolean isValid(String s) {
 
-        // string에 white space 없다고 가정 (문제 조건 없음)
+        if (s.length() % 2 !=0 ) return false;
 
-        // early return condition
-        if(s.length() % 2 != 0) return false;
+        ArrayDeque<Character> stack = new ArrayDeque<>();
 
-        // set two pointers
-        int left = 0;
-        int right = s.length() - 1;
-
-        while ( left < right ){
-            switch (s.charAt(left)) {
-                case '(' -> { if(s.charAt(right) != ')') return false; }
-                case '{' -> { if(s.charAt(right) != '}') return false; }
-                case '[' -> { if(s.charAt(right) != ']') return false; }
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
             }
-            left++;
-            right--;
+
+            // 이 라인이 없어서 "]]"에서 오류남
+            if (stack.isEmpty()) return false;
+
+            if(c==')') {
+                if(stack.pop() != '(') return false;
+            }else if (c=='}'){
+                if(stack.pop() != '{') return false;
+            }else if (c==']'){
+                if(stack.pop() != '[') return false;
+            }
+
         }
 
-        return true;
-    }
-}
-
-// edge case 발생
-// (1) '()[]{}'
-// (2) '[(){}]'
-// (3) '[()]{}'
-
-// Approach 2 - Hint 참고하고 '()[]{}', '[(){}]', '[()]{}' 이 test case만 보고 코드 작성함
-// Approach 3 - ']]' 이 test case 보고 stack.peek() == null 추가함
-class Solution {
-    public boolean isValid(String s) {
-
-        if(s.length() % 2 != 0) return false;
-
-        Deque<Character> stack = new ArrayDeque<>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
-                stack.push(s.charAt(i));
-            }
-            switch (s.charAt(i)){
-                case ')'-> {
-                    // 원래 stack.peek() null check 안 했었음
-                    if(stack.peek() == null || stack.peek() != '('){
-                        return false;
-                    } else {
-                        stack.pop();
-                    }
-                }
-                case '}'-> {
-                    if(stack.peek() == null || stack.peek() != '{'){
-                        return false;
-                    } else {
-                        stack.pop();
-                    }
-                }
-                case ']'-> {
-                    if(stack.peek() == null || stack.peek() != '['){
-                        return false;
-                    } else {
-                        stack.pop();
-                    }
-                }
-            }
-        }
-
-        if(stack.isEmpty()){
-            return true;
-        }else{
-            return false;
-        }
+        return stack.isEmpty();
 
     }
 }
-
-// edge case 발생
-// (1) ']]'
