5facf8ce9cccb46e85bc2b1e088726eeec3fffe3
devYuraKim <dev.yurakim@gmail.com>
2026-05-18 16:53:42 +0900

    practice: NC150 [14 Greedy] 01. Maximum Subarray (Greedy)

--- exercises/neetcode150/14-greedy/14-01-maximum-subarray.java
+++ exercises/neetcode150/14-greedy/14-01-maximum-subarray.java
@@ -1,6 +1,3 @@
-// Mon May 18
-// 결국에는 Brute Force
-// 21'28''
 class Solution {
     public int maxSubArray(int[] nums) {
 
@@ -8,20 +5,24 @@ class Solution {
         int output = nums[0];
 
         for(int i=0; i<nums.length; i++){
-            curSum = 0;
-            for(int j=i; j<nums.length; j++){
-                curSum += nums[j];
-                if(curSum > output){
-                    output = curSum;
-                }
+            curSum += nums[i];
+
+            if(curSum > output){
+                output = curSum;
+            }
+
+            if(curSum < 0){
+                curSum = 0;
             }
         }
 
         return output;
-
     }
 }
 
-// 2, -3, 4, -2, 2, 1, -1, 4
-// minIdx: 0
-// maxIdx: nums.length-1;
+// curSum = 0, output = -3
+// curSum = -3, output = -3
+
+//[-3]
+// curSum = 0, output = -3
+// curSum = -3 -> true -> curSum = 0, true -> output = 0;
