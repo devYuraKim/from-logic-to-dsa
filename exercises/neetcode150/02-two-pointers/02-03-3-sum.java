ec4441a102b1de2170143ee889af07200eab4ca9
devYuraKim <dev.yurakim@gmail.com>
2026-05-20 16:49:17 +0900

    practice: NC150 [02 Two Pointers] 03. 3 Sum

--- exercises/neetcode150/02-two-pointers/02-03-3-sum.java
+++ exercises/neetcode150/02-two-pointers/02-03-3-sum.java
@@ -1,50 +1,48 @@
-//Sun May 10 2026
+// Wed May 20 2026
+// 25'15'' + LLM
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
 
         Arrays.sort(nums);
-
         List<List<Integer>> output = new ArrayList<>();
+        List<Integer> triplet = new ArrayList<>();
 
-        // [1오류] 순회 i 종료 조건
-        // for(int i=0; i<nums.length-2; i++){
-        for(int i=0; i<nums.length; i++){
-
-            // [2오류] early break
-            // if (nums[i] > 0) break;
-
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
             int left = i+1;
             int right = nums.length-1;
-
-            // [3오류] nums[i] 중복 처리
-            // if(i > 0 && nums[i]==nums[i-1]) continue;
-
-            while(left < right){
-                if(nums[i]+nums[left]+nums[right] == 0){
-                    // [4오류] Arrays.asList()
-                    //List<Integer> tempList = Arrays.asList(nums[i], nums[left], nums[right]);
-                    List<Integer> tempList = List.asList(nums[i], nums[left], nums[right]);
-                    output.add(new ArrayList<>(tempList));
-                    // [5오류] left, right는 '이후 원소'와 비교, i는 '이전 원소'와 비교
-                    //while(left < right && nums[left]==nums[left+1]
-                    while(left < right && nums[left-1]==nums[left]) {
-                        left++;
-                    }
-                    left++;
-                    while(left < right && nums[right-1]==nums[right]) {
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum>0){
+                    while(nums[right]==nums[right-1]){
                         right--;
                     }
                     right--;
-                }else if(nums[i]+nums[left]+nums[right] > 0){
-                    right--;
-                }else{
+                }else if(sum<0){
+                    while(nums[left]==nums[left+1]){
+                        left++;
+                    }
                     left++;
+                }else{
+                    output.add(List.of(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
                 }
-            }
-
+            };
         }
 
         return output;
 
     }
 }
+
+// for loop으로 i=0..length-1까지 전진시키면서 duplicate 제외
+// (변경) nums sort해서 중복 처리 가능하도록 + for loop으로 i=0부터 length-1까지 전진시키면서 duplicate 제외
+// [-4, -1, -1, 0, 1, 2]
+// i=0 left=i+1=1, right=length-1
+//     while(left<right)
+//     nums[i]+nums[left]+nums[right]<0: left++;
+//     nums[i]+nums[left]+nums[right]>0: right--;
+//     nums[i]+nums[left]+nums[right]=0: add하고 continue? left++, right--
