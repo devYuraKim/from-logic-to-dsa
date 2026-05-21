573136a86c96a7c63503598954594ef9407ddfca
devYuraKim <dev.yurakim@gmail.com>
2026-05-22 01:49:07 +0900

    practice: NC150 [04 Binary Search] 02. Search a 2D Matrix (Code Kata)

--- exercises/neetcode150/04-binary-search/04-02-search-a-2d-matrix.java
+++ exercises/neetcode150/04-binary-search/04-02-search-a-2d-matrix.java
@@ -1,60 +1,34 @@
-// Mon May 11 2026
-// 44:36
-
+// Fri May 22 2026
+// 07'36''
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
         int rowCount = matrix.length;
         int colCount = matrix[0].length;
 
-        int left = 1;
-        int right = rowCount * colCount;
+        int total = rowCount * colCount;
+        // no need for long becuase 1 <= m, n <= 100
 
-        // 각 칸의 좌표 어떻게 구하지?
-        // (0,0) (0,1) (0,2) (0,3)
-        // (1,0) (1,1) (1,2) (1,3)
+        int left = 0;
+        int right = total-1;
 
+        while(left<=right){
+            int mid = (left+right)/2;
+            //row index: number/colCount 몫
+            //col index: number%colCount 나머지
 
-        // int midRow = (0+rowCount-1)/2;
-        // int midCol = (0+colCount-1)/2;
+            int rowIndex = mid/colCount;
+            int colIndex = mid%colCount;
 
-        // if(target == matrix[midRow][midCol]){ //matrix[1][1]
-        //     return true;
-        //     // 6번째라면 
-        //     // 6/colCount = 몫 = row
-        //     // 6/colCount = 나머지 > 1 때는 -1, 아닐 때는 마지막 column = col
-        // }else if(target > matrix[midRow][midCol]){
-        //     midRow = (midRow+rowCount-1)/2;
-        // }
-
-
-        // cellNumber -> matrix[r][c]
-        // (1) row: cellNumber / colCount;
-        // (2) col: cellNumber % colCount;
-        //          if(cellNumber%colCount==0) col=matrix[0].length-1;
-        //          else col=cellNumber%colCount-1;
-
-
-        while(left <= right){
-            int half = (left+right) / 2;
-
-            int row = half % colCount == 0 ? half/colCount - 1 : half / colCount;
-            int col = half % colCount == 0 ? colCount-1 : half % colCount -1;
-
-            if(target == matrix[row][col]){
+            if(matrix[rowIndex][colIndex] < target){
+                left=mid+1;
+            }else if(matrix[rowIndex][colIndex] > target){
+                right=mid-1;
+            }else{
                 return true;
-            }else if(target > matrix[row][col]){
-                left = half+1;
-                // half = (left+right)/2;
-            }else {
-                right = half-1;
-                // half = (left+right)/2;
             }
         }
 
         return false;
-
     }
 }
-
-// m * n = r * c
