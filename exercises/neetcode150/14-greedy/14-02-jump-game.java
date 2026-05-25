e922143439d1e7da99b3e8dc007a8e88218476a0
devYuraKim <dev.yurakim@gmail.com>
2026-05-25 19:40:33 +0900

    practice: NC150 [14 Greedy] 02. Jump Game

--- exercises/neetcode150/14-greedy/14-02-jump-game.java
+++ exercises/neetcode150/14-greedy/14-02-jump-game.java
@@ -1,40 +1,29 @@
-// Tue May 19 2026
-// 13'51''
-
+// Sun May 24 2026
+// 7'30''
 class Solution {
     public boolean canJump(int[] nums) {
 
-        int maxIndex = 0;
-
-        for(int i=0; i<nums.length-1; i++){
-
-            int possibleLimit = i+nums[i];
-
-            if(possibleLimit == i){
-                if(maxIndex == i){
-                    return false;
-                }
-                continue;
-            }
-            if(possibleLimit > maxIndex){
-                maxIndex = possibleLimit;
-            }
-
+        int curMax = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (curMax < i) return false;
+            curMax = Math.max(curMax, i + nums[i]);
         }
+        return true;
 
-        return maxIndex >= nums.length-1 ? true: false;
-
+        // int curMax = nums[0];
+        // for(int i=0; i<nums.length-1; i++){
+        //     int nextIndex = i+nums[i];
+        //     if(nextIndex <= i){
+        //         //여기서는 점프 못 함.. 그냥 끼어 있음
+        //         if(curMax <= i){
+        //             return false;
+        //         }else {
+        //             continue;
+        //         }
+        //     }else{
+        //         curMax = Math.max(curMax, nextIndex);
+        //     }
+        // }
+        // return true;
     }
 }
-
-// (0,1,2)
-// [0,2,3]
-// <0,3,5>
-
-// (0,1,2,3,4)
-// [1,2,1,0,1]
-// <1,3,3,-,5>
-
-// (0,1,2,3,4)
-// [1,2,0,1,0]
-// <1,3,-,4,->
