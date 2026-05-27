194afd2b76f688087420d590c449976cb97b0a0f
devYuraKim <dev.yurakim@gmail.com>
2026-05-27 19:18:43 +0900

    practice: NC150 [14 Greedy] 01. Maximum Subarray

--- exercises/neetcode150/14-greedy/14-01-maximum-subarray.java
+++ exercises/neetcode150/14-greedy/14-01-maximum-subarray.java
@@ -1,8 +1,5 @@
-// Sun May 24 2026
-// 14'45''
-
-// May 19 07'46''에 비해 늦어지고
-// if-if 처리해야 하는데 if-else if로 처리함 (여기서 문제는 없음)
+// Wed May 27 2026
+// 5'48''
 class Solution {
     public int maxSubArray(int[] nums) {
 
@@ -12,14 +9,16 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             curSum += nums[i];
 
-            //
-            if(output < curSum){
-                output = curSum;
-            }else if(curSum < 0){
+            output = Math.max(output, curSum);
+
+            if(curSum < 0){
                 curSum = 0;
+                continue;
             }
         }
 
         return output;
+
     }
 }
+
