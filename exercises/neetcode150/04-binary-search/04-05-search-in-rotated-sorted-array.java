5bf3be96e3df5b0ead47eadb73273494548528d5
devYuraKim <dev.yurakim@gmail.com>
2026-05-30 00:52:57 +0900

    practice: NC150 [04 Binary Search] 05. Search in Rotated Sorted Array

--- exercises/neetcode150/04-binary-search/04-05-search-in-rotated-sorted-array.java
+++ exercises/neetcode150/04-binary-search/04-05-search-in-rotated-sorted-array.java
@@ -1,35 +1,37 @@
-// Fri May 29 2026
-// 43'32'' + LLM Feedback
+// Sat May 30 2026
+// 29'53'' + LLM feedback
 class Solution {
     public int search(int[] nums, int target) {
 
         int left = 0;
         int right = nums.length-1;
 
-        while(left<=right){
+        // ####오류1####
+        while(left <= right){
+            //while(left<right){
             int mid = left + (right-left)/2;
 
             if(nums[mid]==target){
                 return mid;
             }
 
-            if(nums[mid]<nums[right]){ //오름차순 배열
-                if(target < nums[mid] || target > nums[right]){ //target can't be between [mid] and [right]
+            // ####오류2####
+            if(nums[mid] <= nums[right]){
+                //if(nums[mid] < nums[right]){ //mid부터 right까지 ascending order
+                // target은 mid보다 작거나, right보다 큰 경우 -> mid부터 right에 존재하지 않음 -> mid-right 날림 = right=mid-1
+                if(target < nums[mid] || target > nums[right]){
                     right = mid-1;
-                }else{ // target > nums[mid] || target < nums[right]
-                    // [1,2,3,4,5,6] target=5
-                    // left=0, right=5, mid=2
-                    // nums[mid2]=3, nums[right]=6 target=5
+                    // target이 mid보다 크거나(같은 경우는 위에서 handle), right보다 같거나 작은 경우 -> left-mid 날림 -> left=mid+1
+                }else{ // target >= mid && target <= nums[right]
                     left = mid+1;
                 }
-            }else{ // nums[mid]>nums[right] 중간에 푹 꺼지는 부분 존재 -> nums[left]부터 nums[mid]까지는 오름차순 배열
-                // [3,4,5,6,0,1,2] target=4
-                // left=0, right=6, mid=3
-                // nums[mid3]=6, nums[right6]=2
-                if(target >= nums[left] && target < nums[mid]){
-                    right= mid-1;
-                }else { //target <= nums[left] && target > [mid] -> target은 nums[left]부터 nums[mid]까지는 없음
-                    left = mid+1;
+            }else{ //left부터 mid까지 ascending order (mid부터 right 중에 dip이 존재함) [3,4,5,0,1,2]
+                // target이 left보다 작거나, mid보다 큰 경우 -> left부터 mid에 존재하지 않음 -> left-mid 날림 -> left=mid+1
+                if(target < nums[left] || target > nums[mid]){
+                    left=mid+1;
+                    // target이 left보다 같거나 크거나, mid보다 작은 경우(같은 경우는 위에서 handle) -> mid-right 날림 -> right=mid-1;
+                }else{ // target>=nums[left] && target <= nums[mid]
+                    right=mid-1;
                 }
             }
         }
@@ -39,30 +41,16 @@ class Solution {
     }
 }
 
-// [1] target = 2
-// left=0, right=0, mid=0
-// nums[mid]=nums[right]=1 < target=2
-// left=mid+1=1???
-
-
 // [4,5,6,7,0,1,2] target=0
 // left=0, right=6, mid=3
-// nums[mid3]=7 > nums[right6]=2 < target=0 left=mid+1
+// nums[mid3]=7 > nums[right6]=2 -> target=0 < nums[left0]=4, < nums[mid3]=7 -> left=mid+1=4
 // left=4, right=6, mid=5
-// nums[mid5]=1 < nums[right6]=2, target=0<nums[mid]=1 right=mid-1;
-// left=4, right=4, mid=4
-
-// need to find the exact value -> left<=right ???
-// [3,4,5,6,1,2] target=1
-// left=0, right=5, mid=2
-// nums[mid2]=5 > nums[right5]=2 -> 중간에 푹 꺼지는 부분 존재: target < nums[right]: left=mid+1=3
-// left=3, right=5, mid=4
-// nums[mid4]=1
+// nums[mid5]=1 < nums[right6]=2 -> target=0 < nums[mid5]=1, < nums[right6]=2 -> left=mid+1=6
+// left=6 right=6 (while break) return -1
 
 // [3,5,6,0,1,2] target=4
 // left=0, right=5, mid=2
-// nums[mid2]=6 > nums[right]=2 -> 중간에 푹 꺼지는 부분 존재: target > nums[right]: right=mid
-// left=0, right=2, mid=1
-// nums[mid1]=5 < nums[right2]=6 -> 오름차순 배열: target < nums[mid]: right=mid-1
-// left=0, right=0, mid=0
-// nums[mid0] = 3
+// nums[mid2] = 6 > nums[right5]=2 -> target=4 > nums[mid2]=6 -> left=mid+1=3
+// left=3, right=5, mid=4
+// nums[mid4] = 1 < nums[right5]=2 -> target=4 < nums[right5]2 -> left=mid+1 = 5
+// left=5, right=5 (while break) return -1
