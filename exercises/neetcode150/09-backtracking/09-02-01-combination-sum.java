64b96f262be16954fc27c4f2760a9a4c3a07e71b
devYuraKim <dev.yurakim@gmail.com>
2026-06-01 23:35:53 +0900

    practice: NC150 [09 Backtracking] 02. Combination SUm

--- exercises/neetcode150/09-backtracking/09-02-01-combination-sum.java
+++ exercises/neetcode150/09-backtracking/09-02-01-combination-sum.java
@@ -1,71 +1,33 @@
-// try1
+// Mon Jun 1 2026
 class Solution {
     public List<List<Integer>> combinationSum(int[] nums, int target) {
 
+        List<Integer> combi = new ArrayList<>();
         List<List<Integer>> output = new ArrayList<>();
-        List<Integer> combination = new ArrayList<>();
 
-        recursion(nums, target, output, combination, 0);
+        recursion(nums, combi, output, target, 0);
 
         return output;
-    }
-
-    public void recursion(int[] nums, int target, List<List<Integer>> output, List<Integer> combination, int currentIndex){
-
-        if(target - nums[currentIndex] == 0){
-            output.add(new ArrayList<>(combination));
-            return;
-        }
-
-        if(target - nums[currentIndex] < 0){
-            return;
-        }
-
-        for(int i = currentIndex; i < nums.length; i++){
-            //nums[currentIndex]를 포함한 모든 경우
-            combination.add(nums[currentIndex]);
-            recursion(nums, target-nums[currentIndex], output, combination, currentIndex);
-            //nums[currentIndex]를 포함하지 않도록
-            combination.remove(combination.size()-1);
-        }
 
     }
 
-}
+    private void recursion(int[] nums,  List<Integer> combi, List<List<Integer>> output, int target, int index){
 
-//try2
-// 41:18
-
-class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-
-        List<List<Integer>> output = new ArrayList<>();
-        List<Integer> combination = new ArrayList<>();
-
-        recursion(nums, target, output, combination, 0);
-
-        return output;
-    }
-
-    public void recursion(int[] nums, int target, List<List<Integer>> output, List<Integer> combination, int currentIndex){
-
+        // target==0이 되면
         if(target == 0){
-            output.add(new ArrayList<>(combination));
+            output.add(new ArrayList<>(combi));
             return;
-        }
-
-        if(target < 0){
+        }else if(target < 0 || index == nums.length){
             return;
-        }
+        }else{
+            //해당 원소 포함
+            combi.add(nums[index]);
+            recursion(nums, combi, output, target-nums[index], index);
 
-        for(int i = currentIndex; i < nums.length; i++){
-            //nums[currentIndex]를 포함한 모든 경우
-            combination.add(nums[i]);
-            recursion(nums, target-nums[i], output, combination, i);
-            //nums[currentIndex]를 포함하지 않도록
-            combination.remove(combination.size()-1);
+            //해당 원소 미포함
+            combi.remove(combi.size()-1);
+            recursion(nums, combi, output, target, index+1);
         }
 
     }
-
 }
